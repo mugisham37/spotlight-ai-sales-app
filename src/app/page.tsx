47565b04d9f7ee1d0825d45d5500007c235ce3ee
mugisham37 <mugisham333@gmail.com>
2025-09-01 12:40:19 +0100

    added in the logo ticker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/sections/Navbar";
 import Hero from "@/sections/Hero";
+import LogoTicker from "@/sections/LogoTicker";
 
 export default function Home() {
   return (
     <div className="min-h-screen relative">
       <Navbar />
       <Hero />
+      <LogoTicker />
     </div>
   );
 }
