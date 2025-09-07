3308dcd20b0109319768689e15ff55b882756e5c
mugisham37 <mugisham333@gmail.com>
2025-09-07 11:20:28 +0100

    built the sidebar and header components

---
+++ src/lib/data.ts
@@ -0,0 +1,12 @@
+import { id } from "date-fns/locale";
+import { link } from "fs";
+import { title } from "process";
+
+export const sidebarData = [
+    {
+        id:1,
+        title:"Dashboard",
+        icon:"/assets/icons/dashboard.svg",
+        link:"/home"
+    }
+]
